e331d1afdc812290aa18c344e6710f36121528c4
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-06-09 20:04:48 -0300

    chore: add config eslint and prettier

---
+++ src/pages/_document.jsx
@@ -0,0 +1,25 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
