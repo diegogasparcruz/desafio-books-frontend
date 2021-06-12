35cb43bcec1745aa5da32e7f1151a1bc348f96bb
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-06-11 22:03:52 -0300

    refactor: change signIn method for auth service

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,9 +1,39 @@
-import { destroyCookie, parseCookies } from 'nookies';
+import { destroyCookie, parseCookies, setCookie } from 'nookies';
 import { AUTH_TOKEN, USER_TOKEN } from '../utils/constants';
 import { api } from './api';
 
 export async function signIn({ email, password }) {
-  return api.post('auth/sign-in', { email, password });
+  const response = {
+    data: null,
+    error: null,
+  };
+
+  try {
+    const { data, headers } = await api.post('auth/sign-in', {
+      email,
+      password,
+    });
+
+    response.data = data;
+
+    setCookie(undefined, AUTH_TOKEN, headers.authorization, {
+      maxAge: 60 * 60 * 24, // 1 day
+    });
+
+    setCookie(undefined, USER_TOKEN, JSON.stringify(data), {
+      maxAge: 60 * 60 * 24, // 1 day
+    });
+
+    api.defaults.headers['Authorization'] = `Bearer ${headers.authorization}`;
+  } catch (err) {
+    response.error = 'Erro interno do servidor';
+
+    if (err.response) {
+      response.error = err.response.data.errors.message;
+    }
+  }
+
+  return response;
 }
 
 export function getUser() {
