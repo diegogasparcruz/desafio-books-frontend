0fd10291f90ce2ec980456c7411e983b0a45f4b7
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-06-14 17:48:25 -0300

    style: refactor styles Container of home page

--- src/styles/pages/Home.js
+++ src/styles/pages/Home.js
@@ -1,22 +1,29 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Main = styled.div`
   background-image: url('bg-home.png');
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
   background-attachment: fixed;
+  height: 100vh;
+  overflow: hidden;
+`;
 
-  @media (min-width: 875px) {
-    height: 100vh;
-  }
+export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: auto;
 `;
 
 export const Content = styled.div`
   max-width: 1200px;
+  width: 100%;
   margin: 0 auto;
   padding: 0 1rem;
-
   display: flex;
   flex-direction: column;
 `;
