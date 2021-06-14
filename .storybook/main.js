23bdcb8d322923f8d43f469dbbe38d8edafafb2a
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-06-13 23:26:30 -0300

    chore: add new config storybook with icon svg

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,25 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  webpackFinal: config => {
+    config.resolve.modules = [
+      path.resolve(__dirname, '..', 'src'),
+      'node_modules',
+    ];
+
+    const fileLoaderRule = config.module.rules.find(
+      rule => rule.test && rule.test.test('.svg')
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: 'pre',
+      loader: require.resolve('@svgr/webpack'),
+    });
+
+    return config;
+  },
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+};
